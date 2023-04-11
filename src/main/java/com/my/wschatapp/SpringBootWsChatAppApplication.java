0449da144e4d35e1fef06dd9354c3c3ad640a565
Sem Zhivotov <semzhivotov@gmail.com>
2023-04-11 22:38:28 +0300

    Initial commit

---
+++ src/main/java/com/my/wschatapp/SpringBootWsChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.my.wschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWsChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWsChatAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/my/wschatapp/configuration/WebSocketConfiguration.java
@@ -0,0 +1,4 @@
+package com.my.wschatapp.configuration;
+
+public class WebSocketConfiguratin {
+}
